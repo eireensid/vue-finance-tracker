f3f94122376e065a8d7cad73d84df8af68cec88f
Irina <sidoren-irina@yandex.ru>
2021-10-07 23:51:56 +0300

    navbar dropdown, date filter

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,12 @@
 <template>
   <ul class="sidenav app-sidenav" :class="{open: isOpen}">
-    <router-link v-for="link in links" :key="link.url" tag="li" active-class="active" :to="link.url" :exact="link.exact">
+    <router-link 
+      v-for="link in links" 
+      :key="link.url" 
+      tag="li" 
+      active-class="active" 
+      :to="link.url" 
+      :exact="link.exact">
       <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
     </router-link>
   </ul>
